098b7bc5e8231780699ec2ecee923f0cef767cff
Trook94 <433101456@Student.ksu.edu.sa>
2019-12-05 21:35:45 +0300

    Register a service worker

---
+++ js/registeration.js
@@ -0,0 +1,13 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      if (navigator.serviceWorker) {
+        navigator.serviceWorker.register('/sw.js', {
+          scope: '/'
+        }).then( registration => {
+           console.log('Service worker registeration successful');
+        }).catch(err => {
+           console.log('Service worker registration failed');
+        })
+      }
+    })
+  }
