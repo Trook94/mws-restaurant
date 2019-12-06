27ad1310f756d8ea9c1e6cc1af76e59e381dbe3f
Trook94 <433101456@Student.ksu.edu.sa>
2019-12-06 16:46:50 +0300

    add favicon + fix cache name

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ let staticCacheName = 'mws-static-v1';
 
 self.addEventListener('install', function (event){
     event.waitUntil(caches.open(staticCacheName).then(function(cache){
-        return cache.addAll(['./',
-        'js/main.js', 'js/restaurant_info.js', 'js/dbhelper.js', 'index.html', 'restaurant.html',
+        return cache.addAll(['./', 'index.html' , 'restaurant.html' , 'favicon.ico' ,
+        'js/main.js', 'js/restaurant_info.js', 'js/dbhelper.js', 'js\registeration.js' ,
         'css/styles.css',
         'img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 'img/6.jpg', 'img/7.jpg', 'img/8.jpg', 'img/9.jpg', 'img/10.jpg',
         'data/restaurants.json',
@@ -21,7 +21,7 @@ self.addEventListener('install', function (event){
       caches.keys().then(function(cacheNames) {
         return Promise.all(
           cacheNames.filter(function(cacheName) {
-            return cacheName.startsWith('mws-static-') &&
+            return cacheName.startsWith('mws-static-v1') &&
                    cacheName != staticCacheName;
           }).map(function(cacheName) {
             return caches.delete(cacheName);
